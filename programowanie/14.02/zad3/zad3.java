48641a9d527825c60ef6204fd49e3c220b7e3d9b
antekstt <antekstyczen4@gmail.com>
2025-02-25 12:51:48 +0100

    zad3 upload

---
+++ programowanie/14.02/zad3/zad3.java
@@ -0,0 +1,14 @@
+package zad3;
+import java.util.Scanner;
+public class zad3
+{
+    public ststic void main(String[] args)
+    {
+        Scanner d = new Scanner(System.in);
+        int wiek;
+        System.out.println("Podaj swoj wiek:");
+        wiek=d.nextInt();
+        if(wiek>=18) System.out.println("Jestes pelnoletni");
+        else System.out.println("Nie jestes pelnoletni");
+    }
+}
